346df62100e80aa59f19a06ded5e5ac489300237
Mod <m.moujarkash@alpha-apps.ae>
2023-02-03 09:37:47 +0300

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Marvel_Universe"
+include(":androidApp")
+include(":shared")
